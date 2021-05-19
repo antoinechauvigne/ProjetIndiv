fc4ec50fdf5e6e3d41dc4b384a4f042ca3660601
Antoine Chauvigne <antoine.chauvigne@sciencespo.fr>
2021-05-19 21:46:01 +0200

    V036 formulaire nouveaupost

--- projet/communitymanager/forms.py
+++ projet/communitymanager/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Commentaire
+from .models import Commentaire, Post
 
 
 
@@ -9,3 +9,9 @@ class CommentaireForm(forms.ModelForm):
         fields = ('contenu',)  # on veut utiliser tous les champs de la classe Commenature
     #Pour que la case s'affiche sur plusieurs lignes
     #contenu = forms.CharField(widget=forms.Textarea)
+
+
+class NouveauPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields =('communaute', 'titre', 'description', 'priorite', 'evenementiel', 'date_evenement')
